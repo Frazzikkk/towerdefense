324b3ada5d302bbfbe68c16eb73bad45dce07eca
Frazzikkk <154691360+Frazzikkk@users.noreply.github.com>
2026-03-10 17:33:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -27,16 +27,22 @@ class movesprite(sprite):
         vector = self.direction * self.speed
         self.rect.move_ip(vector)
 
+pygame.init()
 
 window = pygame.Window("towerdefense", window_size)
 surface = window.get_surface()
 clock = pygame.Clock()
+font = pygame.Font(None, 32)
+
 player_image = pygame.Surface([50,50])
 player = sprite([window_size[0]/2, window_size[1]/2],player_image,)
+
 bullets = []
 enemies = []
 
+
 runing = True
+score = 0
 
 while runing:
     for event in pygame.event.get():
@@ -50,20 +56,57 @@ while runing:
             img.fill("red")
             bullet = movesprite(center, img, 7, vector)
             bullets.append(bullet)
+
     if randint(0,100) <= 5:
+        r = randint(1,4)
+        if r == 1:
+            pos = pygame.Vector2(
+                randint(0, window_size[0]), -100,
+            )
+        if r == 2:
+            pos = pygame.Vector2(
+                window_size[0] + 100,
+                randint(0, window_size[1])
+
+            )
+        if r == 3:
+            pos = pygame.Vector2(
+                randint(0, window_size[0]),
+                window_size[1] + 100,
+            )
+        if r == 4:
+            pos = pygame.Vector2(
+                -100, 
+                randint(0, window_size[1])
+            )
         center = pygame.Vector2(player.rect.center)
-        pos = pygame.Vector2(0,0)
+        
         vector = (center - pos).normalize()
         img = pygame.Surface([50,50])
         img.fill("yellow")
-        enemy = movesprite(pos, img, 4, vector)
+        speed = randint(200 ,600) / 100
+        enemy = movesprite(pos, img, speed, vector)
         enemies.append(enemy)
+
     for bullet in bullets:
         bullet.update()
     for enemy in enemies:
         enemy.update()
 
-    
+    for enemy in enemies:
+        for bullet in bullets:
+            if bullet.rect.colliderect(enemy.rect):
+                score += 1
+                enemies.remove(enemy)
+                bullets.remove(bullet)
+                break
+    for enemy in enemies:
+        if player.rect.colliderect(enemy.rect):
+            score = 0
+            enemies.clear()
+            bullets.clear()
+            break
+
     surface.fill("blue")
     
     player.render(surface) 
@@ -72,8 +115,11 @@ while runing:
     for enemy in enemies:
         enemy.render(surface)
     player.render(surface) 
+
+    text = "БАллы: " + str(score)
+    image = font.render(text, True, "black")
+    surface.blit(image, [10, 10])
+
     window.flip()
-
     clock.tick(FPS)
-
 
